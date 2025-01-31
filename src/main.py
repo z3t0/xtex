715388a34209c6e881919b170d491d828031d1ad
Rafi Khan <rafi@rafikhan.io>
2025-01-31 14:06:29 +0000

    wip trying to generate feedback candidates

--- src/main.py
+++ src/main.py
@@ -57,6 +57,36 @@ def query_llm_generate_python(user_prompt, seed=123):
     parsed = json.loads(res.json()["response"])
     return parsed["pythonSource"]
 
+def gen_seed_from_feedback(feedback):
+    new_candidates = []
+
+    for fb in feedback:
+        try:
+            prog_candidate = query_llm_generate_python("write me a python program using the following info. py_source is the current code, and there is pylint feedback. fix the issues that pylint has pointed out\n" + json.dumps(fb))
+            new_candidates.append(prog_candidate)
+        except:
+            pass
+
+    return new_candidates
+
+def evolver_iter(candidates):
+    feedback = []
+    survivors = []
+
+    for c in candidates:
+       score = scoring.score(c)
+       pylint_score = score["pylint"]["score"]
+
+       if pylint_score < 0.01:
+           feedback.append({"py_source": prog_candidate, "pylint": score["pylint"] })
+
+       if pylint_score > 0:
+           survivors.append(prog_candidate)
+
+    if len(survivors) > 2:
+        print("evolve_iter has survivors: " + str(len(survivors)))
+        return survivors
+
 def evolver(req):
     survivors = []
     feedback = []
@@ -76,6 +106,14 @@ def evolver(req):
 
     if len(survivors) == 0:
         print("no survivors left. should go back a generation or incorporate feedback")
+
+        prog_candidates = gen_seed_from_feedback(feedback)
+
+        if len(prog_candidates) > 2:
+            print("has feedback candidates")
+            new_seed = evolver_iter(prog_candidates)
+            print(new_seed)
+
     elif len(survivors) > 5:
         print("has survivors. progress to the next generation")
 
